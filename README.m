bca85f874e8f9d7e4f974384b03a4546abe9b8ec
Niko <you@example.com>
2021-02-17 10:34:31 +0200

    remove dump files and add information to README

--- README.m
+++ README.m
@@ -7,9 +7,8 @@
 				#To run this program:                          #
 				#g++ src/main.cpp -o run -lGLEW -lglut -lGL    #
 				#./run                                         #
-				# (you have to have install glew and freeglut) #
 				################################################
-
+				(if you want to build this program you have to have freeglut and glew installed) 
 
 This program is a realtime ray tracer. It uses openGL for making window and set up everything that is seen in the window. The ray tracing happens in the GLSL-shaders. I have implemented simple shadows, phong shadin and simple reflections and refractions to the ray tracing model. The ray tracer also uses perspective camera.
 
@@ -20,4 +19,4 @@ Development ideas:
 - Better camera (moving camera and a support for moving with WASD-keys and mouse)
 - Implement particle simulation
 - Improve the shading model
-
+- Better dielectric 
